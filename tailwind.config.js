3b1f7f75a25dd14fe41a8d58e7c6d2793b84c8af
shivangichauhan100 <shivangichauhan43788@gmail.com>
2025-02-27 23:02:26 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{html,js}"], // Ensure it watches the correct files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+ 
